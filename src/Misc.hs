8dbd132a0fe5d29c6508f8c2031cc7f37f95d5f9
IchiroKawashima <kawashima.ichiro172@mail.kyutech.jp>
2019-10-18 17:31:58 +0900

    add function to Misc.hs

--- src/Misc.hs
+++ src/Misc.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE Rank2Types #-}
+
 module Misc
-    ( topAdderTree
+    ( msu
+    , sft
+    , fadd
+    , fmul
     )
 where
 
@@ -11,13 +17,73 @@ import           Data.Singletons.Prelude        ( TyFun
                                                 , type (@@)
                                                 )
 
-data Sum (n :: Nat) (f :: TyFun Nat Type) :: Type
-type instance Apply (Sum n) l = Signed (n + l)
+{-# ANN msu (defSyn "msu") #-}
+msu :: Unsigned 1024 -> Unsigned 1024
+msu = last . generate t (\x -> resize $ mul x x `mod` resize n)
+  where
+    n :: Unsigned 1024
+    n = 124066695684124741398798927404814432744698427125735684128131855064976895337309138910015071214657674309443149407457493434579063840841220334555160125016331040933690674569571217337630239191517205721310197608387239846364360850220896772964978569683229449266819903414117058030106528073928633017118689826625594484331
+    t :: SNat 10
+    t = SNat
+{-# NOINLINE msu #-}
 
-adderTree :: KnownNat k => Vec (2 ^ k) (Signed n) -> Signed (n + k)
-adderTree = dtfold (Proxy :: Proxy (Sum n)) id $ \_ x y -> add x y
+{-# ANN sft (defSyn "sft") #-}
+sft :: BitVector 32 -> BitVector 5
+sft = shifter
+{-# NOINLINE sft #-}
 
-{-# ANN topAdderTree (defSyn "topAdderTree") #-}
-topAdderTree :: Clock System -> Reset System -> Enable System -> Signal System (Vec 32 (Signed 16)) -> Signal System (Signed 21)
-topAdderTree = exposeClockResetEnable (register 0 . fmap adderTree)
-{-# NOINLINE topAdderTree #-}
+data Shifter (k :: Nat) (f :: TyFun Nat Type) :: Type
+type instance Apply (Shifter k) l = (BitVector l, BitVector (2 ^ (k - l)))
+
+shifter :: (KnownNat k) => BitVector (2 ^ k) -> BitVector k
+shifter x = fst $ dfold' (Proxy :: Proxy (Shifter k)) f (def, x) $ repeat ()
+  where
+    f SNat () (shift, remnant)
+        | bitToBool $ reduceOr lowerBits = (shift ++# 0, lowerBits)
+        | otherwise                      = (shift ++# 1, higherBits)
+        where (higherBits, lowerBits) = split remnant
+
+{-# ANN fadd (defSyn "fadd") #-}
+fadd :: Vec 8 (Signed 32) -> Vec 1 (Signed 35)
+fadd = foldAdd
+{-# NOINLINE fadd #-}
+
+data FoldAdd (k :: Nat) (n :: Nat) (f :: TyFun Nat Type) :: Type
+type instance Apply (FoldAdd k n) l = Vec (2 ^ (k - l)) (Signed (n + l))
+
+foldAdd :: (KnownNat k) => Vec (2 ^ k) (Signed n) -> Vec 1 (Signed (n + k))
+foldAdd xs = dfold' (Proxy :: Proxy (FoldAdd k n)) f xs $ repeat ()
+    where f SNat () = uncurry (zipWith add) . splitAtI
+
+{-# ANN fmul (defSyn "fmul") #-}
+fmul :: Vec 8 (Signed 32) -> Vec 1 (Signed 256)
+fmul = foldMul
+{-# NOINLINE fmul #-}
+
+data FoldMul (k :: Nat) (n :: Nat) (f :: TyFun Nat Type) :: Type
+type instance Apply (FoldMul k n) l = Vec (2 ^ (k - l)) (Signed (n * 2 ^ l))
+
+foldMul :: (KnownNat k) => Vec (2 ^ k) (Signed n) -> Vec 1 (Signed (n * 2 ^ k))
+foldMul xs = dfold' (Proxy :: Proxy (FoldMul k n)) f xs $ repeat ()
+    where f SNat () = uncurry (zipWith mul) . splitAtI
+
+dfold'
+    :: forall p k a
+     . (KnownNat k)
+    => Proxy (p :: TyFun Nat Type -> Type)
+    -> (  forall l
+        . (KnownNat l, l <= k - 1, l + 1 <= k)
+       => SNat l
+       -> a
+       -> p @@ l
+       -> p @@ (l + 1)
+       )
+    -> (p @@ 0)
+    -> Vec k a
+    -> (p @@ k)
+dfold' Proxy f z = go
+  where
+    go :: (KnownNat n, n <= k) => Vec n a -> p @@ n
+    go Nil           = z
+    go (y `Cons` ys) = f (lengthS ys) y (go ys)
+{-# NOINLINE dfold' #-}
